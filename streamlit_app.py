1c782d48d46ccd59bec5d58042159044c614c9ca
mukGH123 <143869357+mukGH123@users.noreply.github.com>
2024-09-15 10:56:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,22 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import pyodbc
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# Function to connect to SQL Server and fetch data
+def get_data_from_sql():
+	conn = pyodbc.connect(
+		'DRIVER={ODBC Driver 17 for SQL Server};'
+		'SERVER=202.66.174.120,1232;'
+		'DATABASE=vaagmndb;'
+		'UID=vaagdadbusr;'
+		'PWD=MefrAyu!Uw8they9ru;'
+	)
+	
+	query = "SELECT Employee_Name, Employee_Code, Employee_Gender, Employee_DOB, GrossSalary FROM EmployeeMater"
+	df = pd.read_sql(query, conn)
+	conn.close()
+	return df
+
+# Load the data from SQL Server
+df = get_data_from_sql()
